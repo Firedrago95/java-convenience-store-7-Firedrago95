83e8a23d23e58e4b46a000c758bcdb54c2f196cd
Firedrago95 <yonghwa95@naver.com>
2024-11-07 20:14:49 +0900

    Feat: 멤버십 적용 입력 기능  구현

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -11,8 +11,10 @@ public class InputView {
         = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
     public static final String ADD_COUNT_MESSAGE
         = "현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)%n";
-    public static final String SUBSTRACT_COUNT_MESSAGE
+    public static final String SUBTRACT_COUNT_MESSAGE
         = "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)%n";
+    public static final String MEMBERSHIP_MESSAGE = "멤버십 할인을 받으시겠습니까? (Y/N)";
+    public static final String RE_PURCHASE_MESSAGE = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
 
     public static Map<String, Integer> readOrder() {
         System.out.println(ORDER_INPUT_MESSAGE);
@@ -26,10 +28,16 @@ public class InputView {
         return checkYesOrNo();
     }
 
-    public static boolean readSubStractCount(String name, Integer count) {
-        System.out.printf(SUBSTRACT_COUNT_MESSAGE, name, count);
+    public static boolean readSubtractCount(String name, Integer count) {
+        System.out.printf(SUBTRACT_COUNT_MESSAGE, name, count);
         return checkYesOrNo();
     }
+
+    public static boolean askForMemberShip() {
+        System.out.println(MEMBERSHIP_MESSAGE);
+        return checkYesOrNo();
+    }
+
 
     private static boolean checkYesOrNo() {
         String input = Console.readLine();
